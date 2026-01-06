5bf8331e0019af2c1eabac9a1315d5c49594b821
finnnpangestu <muhammadalfin975@gmail.com>
2026-01-06 12:49:56 +0700

    fix favicon

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,8 +1,25 @@
+import { Metadata } from "next";
 import "./globals.css";
 
-export const metadata = {
-  title: "Mirai Company Profile",
-  description: "Innovative Solutions for Your Business",
+export const metadata: Metadata = {
+  title: {
+    default: "Mirai Softnet Technology",
+    template: "%s | Mirai Softnet Technology",
+  },
+  description:
+    "Your reliable IT partner delivering innovative, secure, and scalable digital solutions for modern businesses.",
+
+  // TODO: change this!
+  // keywords: [
+  //   "IT Solution",
+  //   "Software Development",
+  //   "Digital Transformation",
+  //   "Technology Partner",
+  //   "Mirai Softnet Technology",
+  // ],
+
+  authors: [{ name: "Mirai Softnet Technology" }],
+  // metadataBase: new URL("https://miraisoftnet.com"),
 };
 
 export default function RootLayout({
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 export default function LandingPage() {
   return (
     <main>
-      <h1>Hello Mirai!</h1>
+      <h1>Hello World!</h1>
       <p>Siap untuk slicing UI kustom.</p>
     </main>
   );
